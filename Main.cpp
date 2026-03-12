549ecf6017e304eaf232f06dfe249f86e4d444fa
Bou7mara <ahmedamineazzouzi.study@gmail.com>
2026-03-12 19:05:13 +0000

    Project 2-2: Construct an XOR Logical Operation

--- Main.cpp
+++ Main.cpp
@@ -2,10 +2,15 @@
 using namespace std;
 int main() {
     cout << "Project 2-1" << endl;
-    double distance = 34000000.0;
-    double lightspeed = 186000.0;
+    double distance = 34000000.0, lightspeed = 186000.0;
     double delay = distance/lightspeed;
-    cout << "In seconds (s), the delay is: " << delay << "\nAnd in minutes, it is: " << delay/60 << "\n";
+    cout << "In seconds (s), the delay is: " << delay << "\nAnd in minutes, it is: " << delay/60 << "\n\n";
 
-
+    cout << "Project 2-2" << endl;
+    bool p, q, XOR;
+    p = true;
+    q = false;
+    if ((p||q)&&(!(p&&q))) XOR = true;
+    else XOR = false;
+    cout << XOR << endl;
 }
