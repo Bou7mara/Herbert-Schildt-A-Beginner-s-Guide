f31b9de6c207bde78e0a84eadb42727e3dcb2a56
Bou7mara <ahmedamineazzouzi.study@gmail.com>
2026-03-12 15:18:16 +0000

    Project 2-1: Talking to Mars

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,11 @@
 #include <iostream>
 using namespace std;
 int main() {
+    cout << "Project 2-1" << endl;
+    double distance = 34000000.0;
+    double lightspeed = 186000.0;
+    double delay = distance/lightspeed;
+    cout << "In seconds (s), the delay is: " << delay << "\nAnd in minutes, it is: " << delay/60 << "\n";
+
+
 }
