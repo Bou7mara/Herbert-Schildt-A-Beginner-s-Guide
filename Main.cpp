b0b2e62b98b2e5a2751d70bef8950d2888eb579c
Azzouzi <ahmedamineazzouzi.study@gmail.com>
2026-03-15 13:15:35 +0000

    M3 MC1

--- Main.cpp
+++ Main.cpp
@@ -54,98 +54,111 @@ int main() {
     //     }
     // }
 
-    char choice;
+    // char choice;
+    //
+    // do {
+    //     // inner loop for validating input
+    //     do {
+    //         cout << "\nSup boss, whatchu need help wiv??\n";
+    //         cout << "a) if\n";
+    //         cout << "b) switch\n";
+    //         cout << "c) for\n";
+    //         cout << "d) while\n";
+    //         cout << "e) do-while\n";
+    //         cout << "f) break\n";
+    //         cout << "g) continue\n";
+    //         cout << "h) goto\n";
+    //         cout << "q) quit\n";
+    //         cout << "make your choice wisely: ";
+    //
+    //         cin >> choice;
+    //
+    //         if(choice!='a' && choice!='b' && choice!='c' && choice!='d' &&
+    //            choice!='e' && choice!='f' && choice!='g' && choice!='h' &&
+    //            choice!='q') {
+    //             cout << "\nBruh, that's not a valid choice. Try again.\n";
+    //         }
+    //
+    //     } while(choice!='a' && choice!='b' && choice!='c' && choice!='d' &&
+    //             choice!='e' && choice!='f' && choice!='g' && choice!='h' &&
+    //             choice!='q');
+    //
+    //     if(choice == 'q') break;
+    //
+    //     cout << "\n";
+    //
+    //     switch(choice) {
+    //         case 'a':
+    //             cout << "if statement syntax:\n\n";
+    //             cout << "if(condition)\n    statement;\n";
+    //             cout << "else\n    statement;\n";
+    //             break;
+    //
+    //         case 'b':
+    //             cout << "switch statement syntax:\n\n";
+    //             cout << "switch(expression) {\n";
+    //             cout << "    case constant:\n";
+    //             cout << "        statement sequence\n";
+    //             cout << "        break;\n";
+    //             cout << "    // ... more cases\n";
+    //             cout << "}\n";
+    //             break;
+    //
+    //         case 'c':
+    //             cout << "for loop syntax:\n\n";
+    //             cout << "for(initialization; condition; update) {\n";
+    //             cout << "    statement;\n";
+    //             cout << "}\n";
+    //             break;
+    //
+    //         case 'd':
+    //             cout << "while loop syntax:\n\n";
+    //             cout << "while(condition) {\n";
+    //             cout << "    statement;\n";
+    //             cout << "}\n";
+    //             break;
+    //
+    //         case 'e':
+    //             cout << "do-while loop syntax:\n\n";
+    //             cout << "do {\n";
+    //             cout << "    statement;\n";
+    //             cout << "} while(condition);\n";
+    //             break;
+    //
+    //         case 'f':
+    //             cout << "break statement syntax:\n\n";
+    //             cout << "break;\n";
+    //             break;
+    //
+    //         case 'g':
+    //             cout << "continue statement syntax:\n\n";
+    //             cout << "continue;\n";
+    //             break;
+    //
+    //         case 'h':
+    //             cout << "goto statement syntax:\n\n";
+    //             cout << "goto label;\n";
+    //             cout << "...\n";
+    //             cout << "label:\n";
+    //             cout << "    statement;\n";
+    //             break;
+    //     }
+    //
+    // } while(choice != 'q');
+    //
+    // cout << "\nExiting help system. Later boss.\n";
 
-    do {
-        // inner loop for validating input
-        do {
-            cout << "\nSup boss, whatchu need help wiv??\n";
-            cout << "a) if\n";
-            cout << "b) switch\n";
-            cout << "c) for\n";
-            cout << "d) while\n";
-            cout << "e) do-while\n";
-            cout << "f) break\n";
-            cout << "g) continue\n";
-            cout << "h) goto\n";
-            cout << "q) quit\n";
-            cout << "make your choice wisely: ";
-
-            cin >> choice;
-
-            if(choice!='a' && choice!='b' && choice!='c' && choice!='d' &&
-               choice!='e' && choice!='f' && choice!='g' && choice!='h' &&
-               choice!='q') {
-                cout << "\nBruh, that's not a valid choice. Try again.\n";
-            }
-
-        } while(choice!='a' && choice!='b' && choice!='c' && choice!='d' &&
-                choice!='e' && choice!='f' && choice!='g' && choice!='h' &&
-                choice!='q');
-
-        if(choice == 'q') break;
-
-        cout << "\n";
-
-        switch(choice) {
-            case 'a':
-                cout << "if statement syntax:\n\n";
-                cout << "if(condition)\n    statement;\n";
-                cout << "else\n    statement;\n";
-                break;
-
-            case 'b':
-                cout << "switch statement syntax:\n\n";
-                cout << "switch(expression) {\n";
-                cout << "    case constant:\n";
-                cout << "        statement sequence\n";
-                cout << "        break;\n";
-                cout << "    // ... more cases\n";
-                cout << "}\n";
-                break;
-
-            case 'c':
-                cout << "for loop syntax:\n\n";
-                cout << "for(initialization; condition; update) {\n";
-                cout << "    statement;\n";
-                cout << "}\n";
-                break;
-
-            case 'd':
-                cout << "while loop syntax:\n\n";
-                cout << "while(condition) {\n";
-                cout << "    statement;\n";
-                cout << "}\n";
-                break;
-
-            case 'e':
-                cout << "do-while loop syntax:\n\n";
-                cout << "do {\n";
-                cout << "    statement;\n";
-                cout << "} while(condition);\n";
-                break;
-
-            case 'f':
-                cout << "break statement syntax:\n\n";
-                cout << "break;\n";
-                break;
-
-            case 'g':
-                cout << "continue statement syntax:\n\n";
-                cout << "continue;\n";
-                break;
-
-            case 'h':
-                cout << "goto statement syntax:\n\n";
-                cout << "goto label;\n";
-                cout << "...\n";
-                cout << "label:\n";
-                cout << "    statement;\n";
-                break;
-        }
-
-    } while(choice != 'q');
-
-    cout << "\nExiting help system. Later boss.\n";
+    // int periods = 0;
+    // char ch;
+    //
+    // cout << "Type characters ($ to stop):\n";
+    //
+    // while (cin.get(ch) && ch != '$') {
+    //     if (ch == '.') {
+    //         ++periods;
+    //     }
+    // }
+    //
+    // std::cout << "Number of periods: " << periods << '\n';
 
 }
